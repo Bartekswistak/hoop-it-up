1514248eaec5e08807ced7f647616c2592c7aeab
bartekswistak <bartekswistak24@gmail.com>
2019-06-29 13:11:32 -0700

    working on logging in

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {Route, NavLink, HashRouter} from "react-router-dom";
 import './App.css';
 import Login from "./components/Login";
+import LoginContainer from "./containers/LoginContainer"
 import Signup from "./components/Signup";
 
 
@@ -15,7 +16,7 @@ function App() {
         </h1>
           <h3><NavLink to="/login">Log In</NavLink> Or <NavLink to="/signup">Sign Up</NavLink></h3>
             <div id="content">
-              <Route path="/login" component={Login}/>
+              <Route path="/login" render={()=> <LoginContainer user={this.props.user} />} component={Login}/>
               <Route path="/signup" component={Signup}/>
             </div>
       </header>
---
+++ client/src/containers/LoginContainer.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Login from "../components/Login";
+import { withRouter } from 'react-router-dom';
+
+
+class LoginContainer extends React.Component {
+    constructor(props) {
+      super(props)
+  
+      if (props.user) {
+        alert("You can't login if you are logged in!")
+        props.history.push('/home')
+      }
+    }
+  
+    render() {
+      return <Login />;
+    }
+  }
+  
+  export default withRouter(LoginContainer)
